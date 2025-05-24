641ad91455ee746c18e80251d4ff91f8824853fa
smoodty\smoodty <lo.ratchapol_st@tni.ac.th>
2025-05-24 23:23:11 +0700

    step3: add GIF to base64 function

--- tesla80.py
+++ tesla80.py
@@ -13,3 +13,9 @@ st.set_page_config(page_title="Tesla Dashboard", layout="wide")
 with open("custom_minimal_style.css") as f:
     st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
 
+# === ฟังก์ชันโหลด GIF เป็น base64 ===
+def get_base64_of_gif(file_path):
+    with open(file_path, "rb") as f:
+        data = f.read()
+        encoded = base64.b64encode(data).decode()
+        return f"data:image/gif;base64,{encoded}"
